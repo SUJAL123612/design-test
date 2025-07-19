a069d3745b82432273c7f12dbc299dd97205e9ed
SuJaL3511 <95642419+SUJAL123612@users.noreply.github.com>
2025-07-20 01:11:07 +0530

    fix

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -3,18 +3,18 @@ import Link from "next/link"
 const Navbar = () => {
     return (
         <>
-            <div className="border border-indigo-600 h-24 flex w-full ">
-                <img src="/logo.png" alt="" className="w-40 ml-7 -mt-8 h-40 justify-between mr-[100px]" />
-                <ul className="text-xl mt-5 -mr-8 justify-between">
-                    <Link className="mr-[50px]" href="/HOME">HOME</Link>
-                    <Link className="mr-[50px]" href="/NEW ARRIVALS">NEW ARRIVALS</Link>
-                    <Link className="mr-[50px]" href="/SHOP">SHOP</Link>
-                    <Link className="justify-between mr-[100px]" href="/SUPPORT">SUPPORT</Link>
+            <div className="border border-transparent h-24 flex w-full ">
+                <img src="/logo.png" alt="logo" className="w-40 ml-7 -mt-8 h-40 justify-between mr-[100px]" />
+                <ul className="text-xl mt-[38px] -mr-8 justify-between">
+                    <Link className="mr-[50px] text-gray-700 hover:text-black" href="/home">HOME</Link>
+                    <Link className="mr-[50px] text-gray-700 hover:text-black" href="/new arrivals">NEW ARRIVALS</Link>
+                    <Link className="mr-[50px] text-gray-700 hover:text-black" href="/shop">SHOP</Link>
+                    <Link className="justify-between mr-[100px] text-[#00000099] hover:text-black" href="/support">SUPPORT</Link>
                 </ul>
-                {/* <p className="border border-black rounded-xl text-xl mt-5 -mr-8 pr-5 pl-5 h-[28px] flex"><img src="Vector.svg" className="" alt="" />Search</p> */}
-                <form className="text-xl mt-5 -mr-8 pr-5 pl-5 h-[28px] flex">
-                {/* <img src="Vector.svg" className="" alt="" /> */}
-                <input type="text" placeholder="Search" className=" rounded-xl bg-[Vector.svg]"/>
+                <form className="text-xl mt-[38px] -mr-8 pr-5 pl-5 h-[28px] flex">
+                    <input type="text" placeholder="Search..." className="rounded-xl w-40 pl-10 h-full bg-[url('/Vector.svg')] bg-no-repeat bg-left bg-[length:40px_20px]"/>
+                    <img src="CART.svg" alt="cart" className="ml-[80px] cursor-pointer"/>
+                    <img src="profile.svg" alt="profile" className="ml-[60px] cursor-pointer"/>
                 </form>
             </div>
         </>
---
+++ src/app/home/page.tsx
@@ -0,0 +1,26 @@
+import Navbar from "../components/navbar";
+
+export default function home() {
+    return (
+        <>
+            <Navbar />
+            <div className="h-full flex w-full ">
+                <div className="ml-[120px] ">
+                    <img src="NAME.svg" alt="name" className="ml-[80px] h-24 mt-[50px] mb-5" />
+                    <p className="text-xl ml-[80px] text-wrap  w-63 text-[#00000099] mb-[100px]">Unleash your phonky style. Stand out Be <span className="text-[#FF7F3E] font-bold">bold.</span></p>
+                    <button className="rounded-xl w-38 ml-[80px] pl-7 pt-4 pb-4 font-semibold bg-[#FF7F3E] flex text-lg text-white hover:bg-[#e66f30] cursor-pointer">Explore<img src="arrow.svg" alt="arrow" className="h-[20px] mt-1 ml-2" /></button><button />
+                </div>
+                <div className="ml-[300px]">
+                    <img src="pic1.png" alt="pic" className="h-96" />
+                </div>
+            </div>
+            <div className="border border-red-500 h-50 bg-[#D9D9D9CC] flex">
+                <div className="relative w-[100px] h-[100px] -rotate-[20deg] border-[3px] border-[#FF7F3E] ml-[230px] mt-[50px]">
+                <img src="pic2.png" alt="pic2" className="absolute w-96 h-96 object-contain rotate-[-5deg] -top-4 -mt-[150px]"/>
+                </div>
+                <p className="text-5xl font-light tracking-wide mt-[60px] ml-[50px]">Quality is our first priority</p>
+                <div className="w-[60px] h-[45px] bg-[#FF7F3E] rounded-l-lg rotate-[30deg] ml-[50px] mt-[50px]"></div>
+            </div>
+        </>
+    );
+}
